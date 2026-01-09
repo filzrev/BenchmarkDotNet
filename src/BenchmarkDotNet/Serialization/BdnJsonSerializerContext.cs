63ed4396870896fc26816696604dcadce759f94e
filzrev <103790468+filzrev@users.noreply.github.com>
2026-01-09 14:42:45 +0900

    chore: migrate serializer options to JsonSerializerOptions

--- src/BenchmarkDotNet/Serialization/BdnJsonSerializerContext.cs
+++ src/BenchmarkDotNet/Serialization/BdnJsonSerializerContext.cs
@@ -1,17 +1,8 @@
 ﻿using BenchmarkDotNet.Disassemblers;
-using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace BenchmarkDotNet.Serialization;
 
-[JsonSourceGenerationOptions(
-    PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase,
-    DictionaryKeyPolicy = JsonKnownNamingPolicy.CamelCase,
-    UseStringEnumConverter = true,
-    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault,
-    RespectNullableAnnotations = true,
-    UnmappedMemberHandling = JsonUnmappedMemberHandling.Disallow
-)]
 [JsonSerializable(typeof(ClrMdArgs))]
 [JsonSerializable(typeof(Sharp))]
 [JsonSerializable(typeof(MonoCode))]
