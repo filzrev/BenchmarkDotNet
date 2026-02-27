5619881860f4b1d8f845b978789ca9a7b001af6b
filzrev <103790468+filzrev@users.noreply.github.com>
2026-02-27 10:48:13 +0900

    t
    Revert "t"
    
    This reverts commit cd9c473bb6b85797d6fac7d5727ab2e0ddc596c9.
    
    t

--- tests/BenchmarkDotNet.Tests/XUnit/EnvRequirementChecker.cs
+++ tests/BenchmarkDotNet.Tests/XUnit/EnvRequirementChecker.cs
@@ -1,5 +1,6 @@
 using BenchmarkDotNet.Environments;
 using System;
+using System.ComponentModel;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Security.Principal;
@@ -15,7 +16,7 @@ public static class EnvRequirementChecker
     {
         EnvRequirement.WindowsOnly => RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? null : "Windows-only test",
         EnvRequirement.NonWindows => !RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? null : "Non-Windows test",
-        EnvRequirement.NonWindowsArm => !RuntimeInformation.IsOSPlatform(OSPlatform.Windows) || !IsArm() ? null : "Non-Windows+Arm test",
+        EnvRequirement.NonWindowsArm => !RuntimeInformation.IsOSPlatform(OSPlatform.Windows) || !IsArm() || IsArm() && IsRunningAsX64EmulatedOnArm64() ? null : "Non-Windows+Arm test",
         EnvRequirement.NonLinux => !RuntimeInformation.IsOSPlatform(OSPlatform.Linux) ? null : "Non-Linux test",
         EnvRequirement.FullFrameworkOnly => BdnRuntimeInformation.IsFullFramework ? null : "Full .NET Framework-only test",
         EnvRequirement.NonFullFramework => !BdnRuntimeInformation.IsFullFramework ? null : "Non-Full .NET Framework test",
@@ -36,4 +37,27 @@ public static class EnvRequirementChecker
 
     private static bool IsArm()
         => BdnRuntimeInformation.GetCurrentPlatform() is Platform.Arm64 or Platform.Arm or Platform.Armv6;
+
+    private static bool IsRunningAsX64EmulatedOnArm64()
+    {
+        if (!IsWow64Process2(
+            System.Diagnostics.Process.GetCurrentProcess().Handle,
+            out ushort processMachine,
+            out ushort nativeMachine))
+        {
+            throw new Win32Exception(Marshal.GetLastWin32Error());
+        }
+
+        return nativeMachine == IMAGE_FILE_MACHINE_ARM64 &&
+               processMachine == IMAGE_FILE_MACHINE_AMD64;
+    }
+
+    private const ushort IMAGE_FILE_MACHINE_AMD64 = 0x8664;
+    private const ushort IMAGE_FILE_MACHINE_ARM64 = 0xAA64;
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    private static extern bool IsWow64Process2(
+        IntPtr hProcess,
+        out ushort processMachine,
+        out ushort nativeMachine);
 }
