307f2fffc6f1875c0f3a158b14ac683506d44efe
filzrev <103790468+filzrev@users.noreply.github.com>
2026-03-04 02:53:01 +0900

    chore: disable parallel tests for manual running test project

---
+++ tests/BenchmarkDotNet.IntegrationTests.ManualRunning/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Runtime.InteropServices;
+using Xunit;
+
+[assembly: Guid("9cfd0c6b-976b-4b89-b97b-7a06a20ab0ed")]
+
+[assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly, DisableTestParallelization = true)]
