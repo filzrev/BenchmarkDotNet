d03eb6591e17ff9b21f7ba5c41b1105b44b4b25c
filzrev <103790468+filzrev@users.noreply.github.com>
2026-04-29 14:13:55 +0900

    test123

--- tests/BenchmarkDotNet.IntegrationTests/DisassemblyDiagnoserTests.cs
+++ tests/BenchmarkDotNet.IntegrationTests/DisassemblyDiagnoserTests.cs
@@ -13,6 +13,8 @@ using BenchmarkDotNet.Tests.Loggers;
 using BenchmarkDotNet.Toolchains;
 using BenchmarkDotNet.Toolchains.CsProj;
 using BenchmarkDotNet.Toolchains.InProcess.Emit;
+using Gee.External.Capstone;
+using Gee.External.Capstone.Arm64;
 using System.Runtime.CompilerServices;
 using System.Text;
 using Xunit.Abstractions;
@@ -94,6 +96,40 @@ namespace BenchmarkDotNet.IntegrationTests
             }
 
             [MethodImpl(MethodImplOptions.NoInlining)] public void Benchmark(bool justAnOverload) { } // we need to test overloads (#562)
+        }
+
+        [Fact]
+        [Trait(Constants.Category, Constants.BackwardCompatibilityCategory)]
+        public void Test123()
+        {
+            // Creating the disassembler in a "using" statement ensures that resources get cleaned up automatically
+            // when it is no longer needed.
+            const Arm64DisassembleMode disassembleMode = Arm64DisassembleMode.Arm;
+            using (CapstoneArm64Disassembler disassembler = CapstoneDisassembler.CreateArm64Disassembler(disassembleMode))
+            {
+                // ....
+                //
+                // Enables disassemble details, which are disabled by default, to provide more detailed information on
+                // disassembled binary code.
+                disassembler.EnableInstructionDetails = true;
+                disassembler.DisassembleSyntax = DisassembleSyntax.Intel;
+
+                var binaryCode = new byte[] {
+                    0x09, 0x00, 0x38, 0xd5, 0xbf, 0x40, 0x00, 0xd5, 0x0c, 0x05, 0x13, 0xd5, 0x20, 0x50, 0x02, 0x0e,
+                    0x20, 0xe4, 0x3d, 0x0f, 0x00, 0x18, 0xa0, 0x5f, 0xa2, 0x00, 0xae, 0x9e, 0x9f, 0x37, 0x03, 0xd5,
+                    0xbf, 0x33, 0x03, 0xd5, 0xdf, 0x3f, 0x03, 0xd5, 0x21, 0x7c, 0x02, 0x9b, 0x21, 0x7c, 0x00, 0x53,
+                    0x00, 0x40, 0x21, 0x4b, 0xe1, 0x0b, 0x40, 0xb9, 0x20, 0x04, 0x81, 0xda, 0x20, 0x08, 0x02, 0x8b,
+                    0x10, 0x5b, 0xe8, 0x3c
+                };
+
+                Arm64Instruction[] instructions = disassembler.Disassemble(binaryCode);
+                foreach (Arm64Instruction instruction in instructions)
+                {
+                    // ...
+                    //
+                    // Do your magic!
+                }
+            }
         }
 
         [Theory]
