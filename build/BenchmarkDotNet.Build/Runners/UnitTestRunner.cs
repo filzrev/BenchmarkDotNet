d91043eea9c53d9913ac2cc40d3207430f645291
filzrev <103790468+filzrev@users.noreply.github.com>
2026-05-28 08:19:13 +0900

    t

--- build/BenchmarkDotNet.Build/Runners/UnitTestRunner.cs
+++ build/BenchmarkDotNet.Build/Runners/UnitTestRunner.cs
@@ -3,6 +3,7 @@ using Cake.Common;
 using Cake.Common.Diagnostics;
 using Cake.Common.Tools.DotNet;
 using Cake.Common.Tools.DotNet.Test;
+using Cake.Core;
 using Cake.Core.IO;
 using System.Runtime.InteropServices;
 
@@ -33,7 +34,7 @@ public class UnitTestRunner(BuildContext context)
     private DirectoryPath TestOutputDirectory { get; } = context.RootDirectory
         .Combine("TestResults");
 
-    private DotNetTestSettings GetTestSettingsParameters(FilePath logFile, string tfm)
+    private DotNetTestSettings GetTestSettingsParameters(FilePath trxFile, string tfm)
     {
         var settings = new DotNetTestSettings
         {
@@ -41,7 +42,13 @@ public class UnitTestRunner(BuildContext context)
             Framework = tfm,
             NoBuild = true,
             NoRestore = true,
-            Loggers = new[] { "trx", $"trx;LogFileName={logFile.FullPath}", "console;verbosity=detailed" },
+            Loggers =
+            [
+                "trx",
+                $"trx;LogFileName={trxFile.FullPath}",
+                $"html;LogFileName={trxFile.ChangeExtension(".html")}",
+                "console;verbosity=detailed"
+            ],
             EnvironmentVariables =
             {
                 ["Platform"] = "" // force the tool to not look for the .dll in platform-specific directory
@@ -54,7 +61,7 @@ public class UnitTestRunner(BuildContext context)
     {
         var os = Utils.GetOs();
         var arch = RuntimeInformation.OSArchitecture.ToString().ToLower();
-        var trxFileName = $"{os}({arch})-{alias}-{tfm}.trx";
+        var trxFileName = $"{os}-{arch}-{alias}-{tfm}.trx";
         var trxFile = TestOutputDirectory.CombineWithFilePath(trxFileName);
         var settings = GetTestSettingsParameters(trxFile, tfm);
 
