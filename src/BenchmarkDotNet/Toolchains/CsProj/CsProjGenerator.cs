418cd89a9ff90810519d0dea075c972ccbcd2f75
filzrev <103790468+filzrev@users.noreply.github.com>
2026-06-07 20:49:00 +0900

    t

--- src/BenchmarkDotNet/Toolchains/CsProj/CsProjGenerator.cs
+++ src/BenchmarkDotNet/Toolchains/CsProj/CsProjGenerator.cs
@@ -211,7 +211,7 @@ namespace BenchmarkDotNet.Toolchains.CsProj
             //}
 
             // Remove ProjectReference node of benchmark project after gathered dll references are added.
-            doc.Root.Descendants("ProjectReference").Single().Remove();
+            doc.Root!.Descendants("ProjectReference").Single().Remove();
 
             using var projectStream = File.Create(artifactsPaths.ProjectFilePath);
 #if NETSTANDARD2_0
