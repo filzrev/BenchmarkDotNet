82ffb61407b4078c0ba1fbc438a8678e162c1ac5
filzrev <103790468+filzrev@users.noreply.github.com>
2026-07-25 04:44:40 +0900

    t

--- tests/BenchmarkDotNet.IntegrationTests/DisassemblyDiagnoserTests.cs
+++ tests/BenchmarkDotNet.IntegrationTests/DisassemblyDiagnoserTests.cs
@@ -39,15 +39,15 @@ namespace BenchmarkDotNet.IntegrationTests
             {
                 // Skip test on `macos(x64)` because test randomly failed on CI.
                 // See: https://github.com/dotnet/BenchmarkDotNet/issues/3086
-                if (RuntimeInformation.GetCurrentPlatform() is Platform.X86 or Platform.X64 && !OsDetector.IsMacOS())
-                {
-                    yield return [Jit.RyuJit, Platform.X64, CsProjCoreToolchain.NetCoreApp10_0]; // .NET Core x64
-                    // We could add Platform.X86 here, but it would make our CI more complicated.
-                }
-                else if (RuntimeInformation.GetCurrentPlatform() is Platform.Arm64)
-                {
-                    yield return [Jit.RyuJit, Platform.Arm64, CsProjCoreToolchain.NetCoreApp10_0]; // .NET Core arm64
-                }
+                //if (RuntimeInformation.GetCurrentPlatform() is Platform.X86 or Platform.X64 && !OsDetector.IsMacOS())
+                //{
+                //    yield return [Jit.RyuJit, Platform.X64, CsProjCoreToolchain.NetCoreApp10_0]; // .NET Core x64
+                //    // We could add Platform.X86 here, but it would make our CI more complicated.
+                //}
+                //else if (RuntimeInformation.GetCurrentPlatform() is Platform.Arm64)
+                //{
+                //    yield return [Jit.RyuJit, Platform.Arm64, CsProjCoreToolchain.NetCoreApp10_0]; // .NET Core arm64
+                //}
             }
 
             // we could add new object[] { Jit.Llvm, Platform.X64, new MonoRuntime() } here but our CI would need to have Mono installed..
