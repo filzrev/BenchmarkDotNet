0ca5d3f4e5f3258bde9907f4108e0a09687d7d71
filzrev <103790468+filzrev@users.noreply.github.com>
2026-09-05 06:12:39 +0900

    chore: modify dummy base address to fix failed tests on macos

--- tests/BenchmarkDotNet.Tests/Disassemblers/Arm64/Arm64DisassemblerTests.cs
+++ tests/BenchmarkDotNet.Tests/Disassemblers/Arm64/Arm64DisassemblerTests.cs
@@ -2,7 +2,8 @@ namespace BenchmarkDotNet.Tests.Disassemblers.Arm64;
 
 public partial class Arm64DisassemblerTests : Arm64DisassemblerTestBase
 {
-    private const ulong DummyBaseAddress = 0x1000_0000_0000_0000UL;
+    // On macos(arm64), available virtual address space is 48-bit (or 52-bit)
+    private const ulong DummyBaseAddress = 0x0000_F000_0000_0000UL;
     private const string DummyTargetFramework = "net10.0";
 
     public Arm64DisassemblerTests(ITestOutputHelper output)
